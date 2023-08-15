9a9a1e332bf19aa74bfb81e3ab90a236ee545530
Varenik575 <96955683+Varenik575@users.noreply.github.com>
2023-08-15 09:37:59 +0000

    Deploying to gh-pages from @ Varenik575/goit-js-hw-09@1e9e4d8336b481389e39a2eac522fb37740eb57b 🚀

---
+++ 01-color-switcher.06e20fc5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(a){e.disabled=!0,t.disabled=!1,timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(function(d){e.disabled=!1,t.disabled=!0,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.06e20fc5.js.map
